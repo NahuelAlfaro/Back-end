1097ad1461d6673757234ba8c26908fe4c9ab4c9
NahuelAlfaro <98058039+NahuelAlfaro@users.noreply.github.com>
2023-05-29 00:44:56 -0300

    arreglando el deployx4

--- src/main/java/com/portfolio/pf/Entity/Persona.java
+++ src/main/java/com/portfolio/pf/Entity/Persona.java
@@ -3,45 +3,44 @@ package com.portfolio.pf.Entity;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import javax.persistence.Id;
-
-
-
-
 
 @Entity
 public class Persona {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int id;
     
     @NotNull
-    @Size(min = 1, max = 50, message = "valor muy corto o muy largo")
+    @Size(min = 1, max = 50, message = "no cumple con la longitud")
     private String nombre;
     
     @NotNull
-    @Size(min = 1, max = 50, message = "valor muy corto o muy largo")
+    @Size(min = 1, max = 50, message = "no cumple con la longitud")
     private String apellido;
     
-    @Size(min = 1, max = 50, message = "valor muy corto o muy largo")
+    @NotNull
+    private String descripcion;
+    
     private String img;
 
     public Persona() {
     }
 
-    public Persona(String nombre, String apellido, String img) {
+    public Persona(String nombre, String apellido, String descripcion, String img) {
         this.nombre = nombre;
         this.apellido = apellido;
+        this.descripcion = descripcion;
         this.img = img;
     }
 
-    public Long getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(int id) {
         this.id = id;
     }
 
@@ -61,6 +60,14 @@ public class Persona {
         this.apellido = apellido;
     }
 
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
     public String getImg() {
         return img;
     }
@@ -68,6 +75,8 @@ public class Persona {
     public void setImg(String img) {
         this.img = img;
     }
+
+    
     
     
     
