b842930787d5e490bb18874e7fc37a9c02b3787b
Nahuel Alfaro <nahuel_alfaro1@outlook.com>
2023-06-03 02:55:07 -0300

    arreglaer el deploy x9

--- src/main/java/com/portfolio/pf/Security/Service/UserDetailsImpl.java
+++ src/main/java/com/portfolio/pf/Security/Service/UserDetailsImpl.java
@@ -2,6 +2,7 @@ package com.portfolio.pf.Security.Service;
 
 import com.portfolio.pf.Security.Entity.Usuario;
 import com.portfolio.pf.Security.Entity.UsuarioPrincipal;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,33 +14,33 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserDetailsImpl implements UserDetailsService {
 
-    @Autowired
-    UsuarioService usuarioService;
-    
-    @Override
-    public UserDetails loadUserByUsername(String nombreUsuario) throws UsernameNotFoundException {
-        Usuario usuario = usuarioService.getByNombreUsuario(nombreUsuario).get();
-        return UsuarioPrincipal.build(usuario);
-    }
-    
-    
-    
-    
-    
-    
-    
-   //private final UsuarioService usuarioService;
-
     //@Autowired
-    //public UserDetailsImpl(UsuarioService usuarioService) {
-      //  this.usuarioService = usuarioService;
-    //}
-
+    //UsuarioService usuarioService;
+    
     //@Override
     //public UserDetails loadUserByUsername(String nombreUsuario) throws UsernameNotFoundException {
-      //  Optional<Usuario> usuarioOptional = usuarioService.getByNombreUsuario(nombreUsuario);
-        //Usuario usuario = usuarioOptional.orElseThrow(() ->
-          //      new UsernameNotFoundException("Usuario no encontrado: " + nombreUsuario));
-        //return UsuarioPrincipal.build(usuario);
+        //Usuario usuario = usuarioService.getByNombreUsuario(nombreUsuario).get();
+      //  return UsuarioPrincipal.build(usuario);
     //}
+    
+    
+    
+    
+    
+    
+    
+   private final UsuarioService usuarioService;
+
+    @Autowired
+    public UserDetailsImpl(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String nombreUsuario) throws UsernameNotFoundException {
+        Optional<Usuario> usuarioOptional = usuarioService.getByNombreUsuario(nombreUsuario);
+        Usuario usuario = usuarioOptional.orElseThrow(() ->
+                new UsernameNotFoundException("Usuario no encontrado: " + nombreUsuario));
+        return UsuarioPrincipal.build(usuario);
+    }
 }
